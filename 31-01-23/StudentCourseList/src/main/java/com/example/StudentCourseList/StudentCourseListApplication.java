8a5d5c6d3809fa9c2b8bd4a26faff7fc80c03a8c
Ashwini <75114642+Ashwini-Patil30@users.noreply.github.com>
2023-01-31 17:24:06 +0530

    Add files via upload

---
+++ 31-01-23/StudentCourseList/src/main/java/com/example/StudentCourseList/StudentCourseListApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentCourseList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCourseListApplication.class, args);
+	}
+
+}
---
+++ 31-01-23/StudentCourseList/src/test/java/com/example/StudentCourseList/StudentCourseListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentCourseList;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
